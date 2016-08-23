19c739c24bd3adaec829bae2ec5c8b755db36477
Stanislav Diamandiev <thefrozen333@abv.bg>
2016-08-23 14:50:17 +0300

    Create Add an Event Form

---
+++ EventTracker/Controllers/EventsController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using EventTracker.Models;
+using EventTracker.ViewModels;
+
+namespace EventTracker.Controllers
+{
+    public class EventsController : Controller
+    {
+        private ApplicationDbContext _context;
+        public EventsController()
+        {
+            _context = new ApplicationDbContext();
+        }
+        // GET: Events
+        public ActionResult Create()
+        {
+            var viewModel = new EventFormViewModel
+            {
+                Categories = _context.Categories.ToList()
+            };
+            return View(viewModel);
+        }
+    }
+}
---
+++ EventTracker/ViewModels/EventFormViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using EventTracker.Models;
+
+namespace EventTracker.ViewModels
+{
+    public class EventFormViewModel
+    {
+        public string Venue { get; set; }
+        public string Date { get; set; }
+        public string Time { get; set; }
+        public int Category { get; set; }
+        public IEnumerable<Category> Categories{ get; set; }
+    }
+}
